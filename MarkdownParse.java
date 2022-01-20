6163a6f8d671abb0987859fe6affbf9697295d48
Anish Devineni <anishcdevineni@gmail.com>
2022-01-20 12:43:11 -0800

    attempted fix open parantheses

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,12 +11,22 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            if (markdown.indexOf(")", currentIndex) == -1) {
+                break;
+            }
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
+            System.out.println(nextOpenBracket);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            int openParen = nextCloseBracket + 1; //markdown.indexOf("(", nextCloseBracket);
+            char possibleOpenParam = markdown.charAt(openParen);
+            
+            if(possibleOpenParam == '(' && markdown.indexOf(")", currentIndex) < markdown.indexOf("[", openParen)){
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            } else {
+                currentIndex++;
+            }
         }
         return toReturn;
     }
